a14e03a1c7965e696f0d5c25f1873f9c44d60f85
robertoabreu <joserobertoabreu1@hotmail.com>
2019-11-02 14:39:24 -0400

    Add Customer Detail screen

---
+++ Profile ShowCase/Views/CustomerDetailViewController/CustomerDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CustomerDetailViewController.swift
+//  Profile ShowCase
+//
+//  Created by Roberto Abreu on 11/2/19.
+//  Copyright © 2019 homeappzz. All rights reserved.
+//
+
+import UIKit
+
+class CustomerDetailViewController: UIViewController {
+    
+    // MARK: - Public Vars
+    
+    // MARK: - Private Vars
+    
+    // MARK: - Init
+    
+    // MARK: - View Lifecycle
+    
+}
+
